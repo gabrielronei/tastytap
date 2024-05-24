c3fd0898a0a03f8095faaadcf88412dcdc69715d
Gabriel Ronei <gabriheron@hotmail.com>
2024-05-23 22:15:56 -0300

    style(product): adicionando docs da atualização de produto

--- src/main/java/br/com/fiap/tastytap/presentation/product/ProductControllerDocs.java
+++ src/main/java/br/com/fiap/tastytap/presentation/product/ProductControllerDocs.java
@@ -2,6 +2,7 @@ package br.com.fiap.tastytap.presentation.product;
 
 import br.com.fiap.tastytap.application.product.SimpleProductView;
 import br.com.fiap.tastytap.presentation.product.create.NewProductForm;
+import br.com.fiap.tastytap.presentation.product.update.UpdateProductForm;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -27,11 +28,21 @@ interface ProductControllerDocs {
     ResponseEntity save(@Valid @RequestBody NewProductForm form);
 
     @Operation(
+            summary = "Altera um produto existente",
+            description = "Altera um produto já cadastrado no sistema")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = SimpleProductView.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos ou incorretos", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "500", description = "Erro interno do sistema", content = {@Content(schema = @Schema())})
+    })
+    ResponseEntity update(@Valid @RequestBody UpdateProductForm form);
+
+    @Operation(
             summary = "Exclui um produto existente",
             description = "Exclui um produto cadastrado no sistema")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Produto excluído com sucesso", content = { @Content(schema = @Schema()) }),
-            @ApiResponse(responseCode = "404", description = "O Id do produto fornecido não foi encontrado", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "200", description = "Produto excluído com sucesso", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "404", description = "O Id do produto fornecido não foi encontrado", content = {@Content(schema = @Schema())}),
     })
     ResponseEntity deleteById(@RequestParam Long id);
 }
